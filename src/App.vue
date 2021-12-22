6d33866a711059792a20bacd030ab8c282ca6da2
RtyXmd <rtyxmd753951@gmail.com>
2021-12-22 16:47:57 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,41 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld msg="test" />
+  <h4>name：{{ testName }}</h4>
+  <h4>count：{{ computeCount }}</h4>
+  <button @click="changeName">changeTestValue</button>
+  <Child :childName="childName" @updateChildName="updateChildName" />
 </template>
 
-<style>
+<script setup>
+import { reactive, toRefs, computed, watch } from "vue";
+import Child from "./components/child.vue";
+const state = reactive({
+  testName: "test",
+  count: 1,
+  childName: "child",
+});
 
-</style>
+const computeCount = computed(() => {
+  return state.count * 2;
+});
+
+watch(
+  () => computeCount.value,
+  (newValue, oldValue) => {
+    console.log(newValue);
+    console.log(oldValue);
+  }
+);
+
+const changeName = () => {
+  state.testName = "tested";
+  state.count = 2;
+};
+
+const updateChildName = (val) => {
+  state.childName = val;
+};
+
+const { testName, childName } = toRefs(state);
+</script>
+
+<style></style>
---
+++ src/components/child.vue
@@ -0,0 +1,20 @@
+<template>
+  <h5>{{ childName }}</h5>
+  <button @click="changeChildName">changeChildName</button>
+</template>
+
+<script setup>
+const props = defineProps({
+  childName: {
+    type: String,
+    default: "",
+  },
+});
+
+const $emit = defineEmits(["updateChildName"]);
+const changeChildName = () => {
+  $emit("updateChildName", "RtyXmd");
+};
+</script>
+
+<style></style>
